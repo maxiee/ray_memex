0ef7b621f0d2f370989ad3a9e00a6fb15d6369eb
Maxiee <maxieewong@gmail.com>
2024-05-26 13:12:32 +0800

    add models

--- docs/前端/窗口系统/状态管理/基于多 Tab 拖拽的窗口系统—状态管理.md
+++ docs/前端/窗口系统/状态管理/基于多 Tab 拖拽的窗口系统—状态管理.md
@@ -1,5 +1,16 @@
 <基于多 Tab 拖拽的窗口系统—状态管理>
-为了实现基于多 Tab 拖拽的窗口系统，我需要有一套响应式的状态管理系统，通过这套系统，我只需要更改状态，前端的窗口就会以响应式做出改变。
+RayMemex 采用类似 VS Code 的多窗口、多 Tab 管理系统。
 
-由于我使用 React 框架，因此我首先需要进行技术选型，我希望选择一种 React 社区内流行的状态管理最佳时间。并为我提供参考实现。
+我使用 react-grid-layout 库进行前端展示，使用 Redux 进行状态管理。
+
+## 实体建模
+
+我使用 TypeScript 进行开发，初衷就是在类型上尽可能得严谨。
+
+因此在本节中，我根据领域建模，设计需要用到的 Model：
+
+- Frame：整个 Electron 窗口是一个 Frame，Frame 包括窗口属性，比如标题、坐标、大小等等。
+- Window：Window 指一个  react-grid-layout 的 GridItem，在 RayMemex 中，它表示一个窗口。
+- Tab：Tab 是位于 Window 中的标签，一个 Window 包含一个或多个 Tab，并且可以切换。同时，Tab 支持拖拽，可以拖到别的 Window 下。同时 Tab 还是有状态的，记录了其内部的 React 组件的实例。
+
 </基于多 Tab 拖拽的窗口系统—状态管理>
---
+++ raymemex-app/renderer/store/model.ts
@@ -0,0 +1,35 @@
+// `Frame` 实体表示整个 Electron 窗口
+interface Frame {
+    id: string;         // Frame 的唯一标识符。
+    title: string;      // Frame 的标题。
+    x: number;          // Frame 的坐标。
+    y: number;
+    width: number;
+    height: number;     // Frame 的大小。
+    windows: Window[];  // Frame 包含的 Window 实例数组。
+}
+
+// `Window` 实体表示一个 react-grid-layout 的 GridItem，即一个窗口
+interface Window {
+    id: string;         // Window 的唯一标识符。
+    frameId: string;    // Window 所属的 Frame 的标识符。
+    x: number;          // Window 在 Frame 中的坐标。
+    y: number;
+    width: number;      // Window 的大小。
+    height: number;
+    tabs: Tab[];        // Window 包含的 Tab 实例数组
+}
+
+// `Tab` 实体表示位于 Window 中的标签
+interface Tab {
+    id: string;         // Tab 的唯一标识符。
+    windowId: string;   // Tab 所属的 Window 的标识符。
+    title: string;      // Tab 的标题。
+    active: boolean;    // 表示 Tab 是否为当前激活的标签。
+    componentInstance: React.ReactNode; // Tab 内部的 React 组件实例。
+}
+
+// 应用程序状态，用于表示整个应用程序的状态，其中包含一个 `frames` 数组，用于存储所有的 Frame 实例。
+interface AppState {
+    frames: Frame[];
+}
