7a5bc6fddf9f4f2d06edffb9d3ec2a96875b0706
Maxiee <maxieewong@gmail.com>
2024-06-01 21:38:04 +0800

    update docs

--- raymemex-app/src/data/file/types/AbstractFileType.ts
+++ raymemex-app/src/data/file/types/AbstractFileType.ts
@@ -7,9 +7,3 @@ enum AbstractFileType {
     Audio = 'Audio',
     // ...
 }
-
-enum SyncStatus {
-    Synced = 'synced', // 已同步
-    Pending = 'pending', // 等待上传
-    Failed = 'failed',  // 同步失败
-}
---
+++ raymemex-app/src/data/file/types/SyncStatus.ts
@@ -0,0 +1,5 @@
+enum SyncStatus {
+    Synced = 'synced', // 已同步
+    Pending = 'pending', // 等待上传
+    Failed = 'failed',  // 同步失败
+}
